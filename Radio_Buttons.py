ca8e821b53a48eba8a5986e0c8a1fd2f7a6ed8ab
jerripat <jerripatterson@gmail.com>
2022-07-12 07:29:09 -0500

    Radio and Checkboxs

--- Radio_Buttons.py
+++ Radio_Buttons.py
@@ -8,9 +8,18 @@ root.geometry("400x400")
 #root.iconbitmap('/guis/tKinter/owl2.ico')
 
 # Radio Buttons
+def radio():
+    if rad.get() == 1:
+          my_label = Label(root, text='You clicked radio button 1').pack(pady=5)
+    else:
+          my_label = Label(root, text='You clicked radio button 2').pack(pady=5) 
+    
+
 rad = IntVar()
 rbutton_1 = Radiobutton(root, text="One", variable=rad, value=1).pack()
 rbutton_2 = Radiobutton(root, text="Two", variable=rad, value=2).pack()
+
+my_button = Button(root, text="Click me", command=radio).pack(pady=20)
 
 
 root.mainloop()
