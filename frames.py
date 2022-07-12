431c37ac35efa654155c9ef93831c86df3a43f69
jerripat <jerripatterson@gmail.com>
2022-07-11 19:10:12 -0500

    Added Radio Buttons

--- frames.py
+++ frames.py
@@ -1,8 +1,25 @@
-from tkinter import *
 
-root = Tk()
-root.title("Frames")
-#root.geometry("400x400")
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+
+root = tk.Tk()
+root.title("Radio Button Demo")
+root.geometry("400x400")
+root.resizable(False, False)
+
+
+def show_selected_size():
+    showinfo(title='Result', message=selected_size.get())
+
+
+selected_size = tk.StringVar()
+sizes = (('Small', 'S'),
+         ('Medium', 'M'),
+         ('Large', 'L'),
+         ('Extra Large', 'XL'),
+         ('Extra Extra Large', 'XXL'))
 
 
 def show():
@@ -14,7 +31,7 @@ def hide():
 
 
 def new():
-    
+
     hide_menu_frames()
     current_status.set("File Status")
 
@@ -38,6 +55,15 @@ def fake_command():
 
 my_menu = Menu(root)
 root.config(menu=my_menu)
+
+current_status = StringVar()
+current_status.set("Waiting")
+rad = IntVar()
+
+# rbutton_1 = tk.RadioButton(root, text="One", variable=rad, value=1)
+# rbutton_2 = tk.RadioButtonGroup(root, text="Two", variable=rad, value=2)
+# rbutton_3 = tk.RadioButton(root, text="Three", variable=rad, value=3)
+
 
 file_menu = Menu(my_menu)
 my_menu.add_cascade(label="File", menu=file_menu)
@@ -71,18 +97,17 @@ choice_menu.add_command(label="Exit", command=root.quit)
 file_frame = Frame(root, width=200, height=200, bd=2,
                    bg="#c9ae63", relief="groove")
 #file_frame.grid(row=1,column=0, columnspan=2, padx=20, pady=20)
-file_frame_label = Label(file_frame, text="File Frame").pack(padx=20, pady=20)
+file_frame_label = Label(file_frame, textvariable="File Frame").pack(padx=20, pady=20)
 
 # Edit Menu Frame
 edit_frame = Frame(root, width=200, height=200, bd=2,
                    bg="#c9ae63", relief="groove")
 #file_frame.grid(row=1,column=0, columnspan=2, padx=20, pady=20)
-edit_frame_label = Label(edit_frame, text="Cut Frame").pack(padx=20, pady=20)
+edit_frame_label = Label(edit_frame, textvariable="Cut Frame").pack(padx=20, pady=20)
 
-current_status = StringVar()
-current_status.set("Waiting")
 
-status_bar = Label(root, textvariable=current_status, bd=2,relief="sunken", width=50).grid(row=3, column=0)
+status_bar = Label(root, textvariable=current_status, bd=2,
+                   relief="sunken", width=50).grid(row=3, column=0)
 
 
 root.mainloop()
