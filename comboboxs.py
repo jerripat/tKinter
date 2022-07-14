94bac682822c3e01ec2c072a63f6276a45d95cb5
jerripat <jerripatterson@gmail.com>
2022-07-14 09:58:08 -0500

    comboboxs

--- comboboxs.py
+++ comboboxs.py
@@ -9,6 +9,10 @@ root.geometry("400x400")
 # root.iconbitmap('owl2.ico')
 # img = PhotoImage()
 
+def select():
+    answer = my_combo.get()
+    my_label = Label(root, text= 'You clicked :' + answer).pack(pady=10)
+
 options = [
 
     'Monday',
@@ -19,9 +23,10 @@ options = [
     'Saturday',
     'Sunday'
 ]
+
 my_combo = ttk.Combobox(root, value=options)
 my_combo.current(0)
 my_combo.pack(pady=10)
-
+my_button = Button(root, text='Select', command=select).pack(pady=10)
 
 root.mainloop()
